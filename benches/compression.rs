09db832a66bf939272c997a34848572c5bdb59ab
Suchith J N <suchithjn22@gmail.com>
2024-10-01 21:46:42 +0200

    benches

--- benches/compression.rs
+++ benches/compression.rs
@@ -1,6 +1,8 @@
+use std::time::{Duration, Instant};
 use criterion::{black_box, Criterion, criterion_group, criterion_main};
 use rand::prelude::{SliceRandom, StdRng};
 use rand::SeedableRng;
+use fpc_compression::compress_into;
 
 fn deterministic_shuffle(vec: &mut Vec<f64>, seed: u64) {
     let mut rng = StdRng::seed_from_u64(seed);
@@ -8,7 +10,8 @@ fn deterministic_shuffle(vec: &mut Vec<f64>, seed: u64) {
 }
 
 fn criterion_compress_ten_million_double_precision_values(c: &mut Criterion) {
-    let table_sizes: Vec<u64> = vec![32, 64, 128, 256, 512, 1024, 2048, 4096];
+    let table_sizes: Vec<u64> = vec![32, 64, 128, 256, 512, 1024, 2048, 4096, 8192,
+                                     8192*2, 8192*4, 8192*8, 8192*16, 8192*32];
     let mut vals = (0..10_000_000).map(|x| x as f64).collect::<Vec<f64>>();
     deterministic_shuffle(&mut vals, 124840); // random seed
     let mut bench_group = c.benchmark_group("compress_10M");
@@ -16,28 +19,24 @@ fn criterion_compress_ten_million_double_precision_values(c: &mut Criterion) {
         bench_group.bench_with_input(
             criterion::BenchmarkId::new("table_size", table_size),
             &table_size,
-            |b, &tsz| { b.iter_with_large_drop(|| black_box(fpc_compression::compress(tsz, &vals))) },
-        );
-    }
-}
-
-fn criterion_compress_hundred_million_double_precision_values(c: &mut Criterion) {
-    let table_sizes: Vec<u64> = vec![32, 64, 128, 256, 512, 1024, 2048, 4096];
-    let mut vals = (0..100_000_000).map(|x| x as f64).collect::<Vec<f64>>();
-    deterministic_shuffle(&mut vals, 124840); // random seed
-    let mut bench_group = c.benchmark_group("compress_100M");
-    for &table_size in &table_sizes {
-        bench_group.bench_with_input(
-            criterion::BenchmarkId::new("table_size", table_size),
-            &table_size,
-            |b, &tsz| { b.iter_with_large_drop(|| black_box(fpc_compression::compress(tsz, &vals))) },
-        );
+            |b, &tsz| { b.iter_custom(|iters| {
+                let mut total_elapsed = Duration::new(0, 0);
+                for _ in 0..iters {
+                    let mut encoding = vec![0_u8; (vals.len() + 1)/2];
+                    let mut residual = Vec::with_capacity(size_of::<f64>() * vals.len());
+                    {
+                        let start = Instant::now();
+                        black_box(compress_into(tsz, &vals, &mut encoding, &mut residual));
+                        total_elapsed += start.elapsed();
+                    }
+                }
+                total_elapsed
+            })});
     }
 }
 
 criterion_group!(benches,
     criterion_compress_ten_million_double_precision_values,
-    // criterion_compress_hundred_million_double_precision_values,
 );
 
 criterion_main!(benches);
--- benches/decompression.rs
+++ benches/decompression.rs
@@ -1,6 +1,10 @@
+use std::time::{Duration, Instant};
+
 use criterion::{black_box, Criterion, criterion_group, criterion_main};
 use rand::prelude::{SliceRandom, StdRng};
 use rand::SeedableRng;
+
+use fpc_compression::decompress_into;
 
 fn deterministic_shuffle(vec: &mut Vec<f64>, seed: u64) {
     let mut rng = StdRng::seed_from_u64(seed);
@@ -8,7 +12,8 @@ fn deterministic_shuffle(vec: &mut Vec<f64>, seed: u64) {
 }
 
 fn criterion_decompress_ten_million_double_precision_values(c: &mut Criterion) {
-    let table_sizes: Vec<u64> = vec![32, 64, 128, 256, 512, 1024, 2048, 4096];
+    let table_sizes: Vec<u64> = vec![32, 64, 128, 256, 512, 1024, 2048, 4096, 8192,
+                                     8192*2, 8192*4, 8192*8, 8192*16, 8192*32];
     let mut vals = (0..10_000_000).map(|x| x as f64).collect::<Vec<f64>>();
     deterministic_shuffle(&mut vals, 124840); // random seed
     let mut bench_group = c.benchmark_group("decompress_10M");
@@ -17,29 +22,24 @@ fn criterion_decompress_ten_million_double_precision_values(c: &mut Criterion) {
         bench_group.bench_with_input(
             criterion::BenchmarkId::new("table_size", table_size),
             &table_size,
-            |b, &tsz| { b.iter_with_large_drop(|| black_box(fpc_compression::decompress(tsz, &compressed))) },
-        );
-    }
-}
-
-fn criterion_decompress_hundred_million_double_precision_values(c: &mut Criterion) {
-    let table_sizes: Vec<u64> = vec![32, 64, 128, 256, 512, 1024, 2048, 4096];
-    let mut vals = (0..100_000_000).map(|x| x as f64).collect::<Vec<f64>>();
-    deterministic_shuffle(&mut vals, 124840); // random seed
-    let mut bench_group = c.benchmark_group("decompress_100M");
-    for &table_size in &table_sizes {
-        let compressed = fpc_compression::compress(table_size, &vals);
-        bench_group.bench_with_input(
-            criterion::BenchmarkId::new("table_size", table_size),
-            &table_size,
-            |b, &tsz| { b.iter_with_large_drop(|| black_box(fpc_compression::decompress(tsz, &compressed))) },
+            |b, &tsz| { b.iter_custom(|iters| {
+                let mut total_elapsed = Duration::new(0, 0);
+                for _ in 0..iters {
+                    let mut result = Vec::with_capacity(vals.len());
+                    {
+                        let start = Instant::now();
+                        black_box(decompress_into(tsz, &compressed, &mut result));
+                        total_elapsed += start.elapsed();
+                    }
+                }
+                total_elapsed
+            })},
         );
     }
 }
 
 criterion_group!(benches,
     criterion_decompress_ten_million_double_precision_values,
-    // criterion_decompress_hundred_million_double_precision_values,
 );
 
 criterion_main!(benches);
